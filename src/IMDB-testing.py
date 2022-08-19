851498b5fe83342ace23bf5320569c838509c5f0
Chris J <89611970+narxlol@users.noreply.github.com>
2022-08-19 14:52:09 -0700

    corrected output with string formatting

--- src/IMDB-testing.py
+++ src/IMDB-testing.py
@@ -13,7 +13,8 @@ def search_movie():
     movies = ia.search_movie(name,10)
     result_list =[]
     for i in range(0,len(movies)):
-       movie_title = movies[i]['title']
+       printIndex = i + 1
+       movie_title = "%-3d %s" % (printIndex,movies[i]['title'])
        movie_id = movies[i].movieID
        result = [movie_title,movie_id]
        result_list.append(result)
@@ -41,6 +42,7 @@ def search_actor():
     headers = ['Person','ID']
     print(tabulate(result_list,headers)+"\n")
     pu.enter_to_continue()
+
 # main loop
 def main():
     
@@ -48,7 +50,6 @@ def main():
     ia = imdb.Cinemagoer()
     query_active = True
     desired_results = 5
-    
     
     # Create the root menu
     menu = ConsoleMenu("IMDB App Menu", "Select option:")
@@ -63,20 +64,6 @@ def main():
     menu.start()
     menu.join()
     
-    #name = input("\nEnter a movie to search: ")
-        
-    # # exit program loop if 'q' is input
-    # if(name == "q"):
-    #     break
-    # collect search results
-    # search = ia.search_movie(name,5)
-        
-    # # print search results - tabulated 
-    # print("\n%.2f Results for [%s]:\n" % (len(search),name))
-    # print(tabulate(search,headers='keys',maxcolwidths=7))
-
- 
-
 if __name__ == "__main__":
     main()
     
