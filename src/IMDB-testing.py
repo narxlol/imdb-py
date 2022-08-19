920cde74d6ebbab2b0a17fdeb799f03237e92229
Chris J <89611970+narxlol@users.noreply.github.com>
2022-08-19 10:40:32 -0700

    clean and comment code

--- src/IMDB-testing.py
+++ src/IMDB-testing.py
@@ -1,10 +1,7 @@
-
-# importing the module
 import imdb
 from tabulate import tabulate
 
 # creating instance of IMDb
-# ia = imdb.IMDb()
 ia = imdb.Cinemagoer()
 query_active = True
 desired_results = 5
@@ -14,27 +11,17 @@ while query_active == True:
     
     name = input("\nEnter a name to search: ")
     
+    # exit program loop if 'q' is input
     if(name == "q"):
         break
     
-    #desired_results = input("Enter the desired search result: ")
-    
-    # searching the name
+    # collect search results
     search = ia.search_movie(name,desired_results)
+    
+    # print search results - tabulated 
     print("\n%.2f Results for [%s]:\n" % (len(search),name))
-    search['cover url'] = ""
     print(tabulate(search,headers='keys',maxcolwidths=7))
-    #printing the resultss
-    for i in range(len(search)):
-        #print(type(search))
-        print(search[i]['title'])
-        #print(tabulate(search[i]),headers='keys')
-        
-        #print("Title %5")
-        #print("%-*s" % search[i]['title'] )
-        headers = ['title','year']
-    #print(tabulate(search,headers="keys",tablefmt='fancy_grid'))
-    #print(tabulate(search,headers=['title','year'],tablefmt='rst',))
+
  
 
 
