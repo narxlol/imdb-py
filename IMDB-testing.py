7316981fddc348bc5dd74961d21664c13152ac36
Chris J <89611970+narxlol@users.noreply.github.com>
2022-08-19 09:40:38 -0700

    initial testing

---
+++ IMDB-testing.py
@@ -0,0 +1,41 @@
+
+# importing the module
+import imdb
+from tabulate import tabulate
+
+# creating instance of IMDb
+# ia = imdb.IMDb()
+ia = imdb.Cinemagoer()
+query_active = True
+desired_results = 5
+
+# main loop
+while query_active == True:
+    
+    name = input("\nEnter a name to search: ")
+    
+    if(name == "q"):
+        break
+    
+    #desired_results = input("Enter the desired search result: ")
+    
+    # searching the name
+    search = ia.search_movie(name,desired_results)
+    print("\n%.2f Results for [%s]:\n" % (len(search),name))
+    search['cover url'] = ""
+    print(tabulate(search,headers='keys',maxcolwidths=7))
+    #printing the resultss
+    for i in range(len(search)):
+        #print(type(search))
+        print(search[i]['title'])
+        #print(tabulate(search[i]),headers='keys')
+        
+        #print("Title %5")
+        #print("%-*s" % search[i]['title'] )
+    headers = ['title','year']
+    #print(tabulate(search,headers="keys",tablefmt='fancy_grid'))
+    #print(tabulate(search,headers=['title','year'],tablefmt='rst',))
+ 
+
+
+    
---
+++ readme.md
@@ -0,0 +1,3 @@
+IMDB Testing App
+
+This app is a test of the IMDB python api wrapper.
